23fdfdea41dcd9079e4d4bbf36029f450ace0d8a
moon <mjj920930@gmail.com>
2020-07-03 01:05:13 +0900

    style: 내용없는 태그 병합

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -16,7 +16,7 @@ const SearchInput = ({ departureStation, arrivalStation, changeStationsInput, se
           />
         </div>
         <div className="w-2/12 h-12 text-center text-gray-800 flex justify-center items-center">
-          <span className="mdi mdi-arrow-right-thick relative bottom-6px text-lg"></span>
+          <span className="mdi mdi-arrow-right-thick relative bottom-6px text-lg"/>
         </div>
         <div className="w-5/12 h-12 text-center text-gray-800">
           <input
