741e8bc059a0433682d3601fb6ef5bc3140e2513
moon <mjj920930@gmail.com>
2020-07-02 21:09:16 +0900

    feat: 출발역과 도착역에 입력한 값을 Search 컴포넌트에서 관리하도록 구현

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { useReducer } from "react";
 import SearchInput from './SearchInput';
 import SearchResult from './SearchResult';
 
 import "../assets/service/css/search.css";
 
-const Search = props => (
-  <>
-    <div className="max-w-sm w-full lg:width-350px rounded bg-white shadow-lg px-6 pt-6 pb-2">
-      <div className="font-bold text-xl mb-4 text-center">지하철 경로 검색</div>
-      <SearchInput />
-    </div>
-    <SearchResult />
-  </>
-);
+function reducer(state, action) {
+  return {
+    ...state,
+    [action.name]: action.value
+  };
+}
+
+const Search = props => {
+  const [state, dispatch] = useReducer(reducer, {
+    departureStation: '',
+    arrivalStation: ''
+  });
+
+  const { departureStation, arrivalStation } = state;
+
+  const handleChange = e => {
+    dispatch(e.target);
+  }
+
+  const searchPath = () => {
+    // axios로 api 데이터 받아오기
+  }
+
+  return (
+    <>
+      <div className="max-w-sm w-full lg:width-350px rounded bg-white shadow-lg px-6 pt-6 pb-2">
+        <div className="font-bold text-xl mb-4 text-center">지하철 경로 검색</div>
+        <SearchInput
+          departureStation={departureStation}
+          arrivalStation={arrivalStation}
+          handleChange={handleChange}
+          searchPath={searchPath}
+        />
+      </div>
+      <SearchResult/>
+    </>
+  );
+};
 
 export default Search;
--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,16 +1,20 @@
 import React from 'react';
 
-const SearchInput = () => {
+
+const SearchInput = ({ departureStation, arrivalStation, handleChange, searchPath }) => {
+
   return (
-    <form className="bg-white mb-4">
+    <>
       <div className="flex flex-wrap mb-3">
         <div className="w-5/12 h-12 text-center text-gray-800">
           <input
             className="appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-full"
             id="departure-station-name"
             type="text"
-            value=""
             placeholder="출발역"
+            name="departureStation"
+            value={departureStation}
+            onChange={handleChange}
           />
         </div>
         <div className="w-2/12 h-12 text-center text-gray-800 flex justify-center items-center">
@@ -21,16 +25,19 @@ const SearchInput = () => {
             className="appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-full"
             id="arrival-station-name"
             type="text"
-            value=""
             placeholder="도착역"
+            name="arrivalStation"
+            value={arrivalStation}
+            onChange={handleChange}
           />
         </div>
       </div>
       <button type="submit" id="search-button"
-              className="w-full text-sm bg-yellow-500 hover:bg-yellow-400 hover:text-gray-700 text-gray-800 font-bold py-2 px-4 rounded-sm">
+              className="w-full text-sm bg-yellow-500 hover:bg-yellow-400 hover:text-gray-700 text-gray-800 font-bold py-2 px-4 rounded-sm"
+              onClick={searchPath}>
         검색
       </button>
-    </form>
+    </>
   );
 };
 
